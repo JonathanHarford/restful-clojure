fe5486226f88d9214431e60db08bc8481ba30aab
Jonathan Harford <jonathanh@mayvenn.com>
2020-03-29 21:32:46 -0700

    Updated some packages

--- restful-clojure/project.clj
+++ restful-clojure/project.clj
@@ -3,14 +3,31 @@
   :url "https://github.com/kendru/restful-clojure"
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [ring/ring-core "1.2.1"]
+
+  ;; :dependencies [[org.clojure/clojure "1.10.0"]
+  ;;                [buddy/buddy-auth "2.2.0"]
+  ;;                [buddy/buddy-hashers "1.4.0"]
+  ;;                [cheshire "5.10.0"]
+  ;;                [compojure "1.6.1"]
+  ;;                [crypto-random "1.2.0"]
+  ;;                [environ "1.1.0"]
+  ;;                [korma "0.5.0-RC1"]
+  ;;                [ragtime "0.3.4"]
+  ;;                [ring/ring-core "1.8.0"]
+  ;;                [ring/ring-defaults "0.3.2"]
+  ;;                [ring/ring-jetty-adapter "1.8.0"]
+  ;;                [ring/ring-json "0.2.0"]
+  ;;                [javax.servlet/servlet-api "2.5"]
+  ;;                [ring/ring-mock "0.4.0"]]
+
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [ring/ring-core "1.8.0"]
                  [ring/ring-jetty-adapter "1.2.1"]
                  [compojure "1.1.6"]
                  [cheshire "5.3.1"]
                  [ring/ring-json "0.2.0"]
                  [korma "0.3.0-RC5"]
-                 [org.postgresql/postgresql "9.2-1002-jdbc4"]
+                 [postgresql "9.3-1102.jdbc41"]
                  [ragtime "0.3.4"]
                  [environ "0.4.0"]
                  [buddy/buddy-hashers "0.4.0"]
@@ -22,8 +39,12 @@
   ; with "lein ring server". It also allows us to package up our application
   ; as a standalone .jar or as a .war for deployment to a servlet contianer
   ; (I know... SO 2005).
+
+  ;; :plugins [[lein-ring "0.12.5"]
+  ;;           [lein-environ "1.1.0"]
+  ;; ]
+
   :plugins [[lein-ring "0.8.10"]
-            [ragtime/ragtime.lein "0.3.6"]
             [lein-environ "0.4.0"]
             [org.clojure/core.unify "0.5.7"]]
 
